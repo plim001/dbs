d2ed039a9b45845b144ac970a30cbc4760b0c7e9
plim001 <limyong91@gmail.com>
2025-07-16 18:07:19 +0800

    nnb

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def index():
     return(render_template("index.html"))
 
 @app.route("/main",methods=["GET","POST"])
-def prediction():
+def main():
     q = float(request.form.get("q"))
     # load model
     model = joblib.load("dbs.jl")
     # make prediction
     pred = model.predict([[q]])
-    return(render_template("prediction.html",r=pred))
+    return(render_template("main.html"))
 
 if __name__ == "__main__":
     app.run()
